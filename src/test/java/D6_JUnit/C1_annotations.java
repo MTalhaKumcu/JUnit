df0de6fd3e9e91f0b18dd3db1ca06e65260624c7
=Talha Kumcu <=mkumcu151@gmail.com>
2023-11-20 11:18:27 +0100

    first

---
+++ src/test/java/D6_JUnit/C1_annotations.java
@@ -0,0 +1,19 @@
+package D6_JUnit;
+
+import org.junit.Test;
+
+public class C1_annotations {
+    @Test
+    public void googleTest(){
+        System.out.println("google");
+    }
+    @Test
+    public void wiseTest(){
+        System.out.println("wise");
+    }
+    @Test
+    public void amazonTest(){
+        System.out.println("amazon");
+    }
+
+}
